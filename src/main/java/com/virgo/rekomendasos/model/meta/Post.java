91620238fbe3120d7f80a17b57d6076045eccce8
vigorjs <111201912289@mhs.dinus.ac.id>
2024-07-23 13:58:59 +0700

    feat: add model entity

---
+++ src/main/java/com/virgo/rekomendasos/model/meta/Post.java
@@ -0,0 +1,40 @@
+package com.virgo.rekomendasos.model.meta;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Table(name = "posts")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title", length = 100, nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "picture")
+    private String picture;
+
+    @Column(name = "star_review", nullable = false)
+    private Integer star_review;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user_id;
+
+    @ManyToOne
+    @JoinColumn(name = "place_id")
+    private Place place_id;
+
+
+}
